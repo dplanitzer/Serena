476a986db282148d148c33a4bb4042b503e5c332
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2024-02-13 23:08:49 -0800

    More limits.h updates

--- Library/libsystem/Headers/System/abi/_limits.h
+++ Library/libsystem/Headers/System/abi/_limits.h
@@ -11,35 +11,46 @@
 
 #include <System/abi/_dmdef.h>
 
+#define BOOL_WIDTH  8
+
+#define CHAR_WIDTH 8
 #define CHAR_BIT 8
 #define CHAR_MIN 0x80
 #define CHAR_MAX 0x7f
 
+#define SCHAR_WIDTH 8
 #define SCHAR_MIN 0x80
 #define SCHAR_MAX 0x7f
 
+#define SHRT_WIDTH 16
 #define SHRT_MIN 0x8000
 #define SHRT_MAX 0x7fff
 
+#define INT_WIDTH __INT_WIDTH
 #define INT_MIN __INT_MIN
 #define INT_MAX __INT_MAX
 
+#define LONG_WIDTH __LONG_WIDTH
 #define LONG_MIN __LONG_MIN
 #define LONG_MAX __LONG_MAX
 
+#define LLONG_WIDTH __LLONG_WIDTH
 #define LLONG_MIN __LLONG_MIN
 #define LLONG_MAX __LLONG_MAX
 
 #define UCHAR_WIDTH 8
-#define USHRT_WIDTH 16
-#define UINT_WIDTH __UINT_WIDTH
-#define ULONG_WIDTH __ULONG_WIDTH
-#define ULLONG_WIDTH __ULLONG_WIDTH
-
 #define UCHAR_MAX 255
+
+#define USHRT_WIDTH 16
 #define USHRT_MAX 65535
+
+#define UINT_WIDTH __UINT_WIDTH
 #define UINT_MAX __UINT_MAX
+
+#define ULONG_WIDTH __ULONG_WIDTH
 #define ULONG_MAX __ULONG_MAX
+
+#define ULLONG_WIDTH __ULLONG_WIDTH
 #define ULLONG_MAX __ULLONG_MAX
 
 #define PTRDIFF_WIDTH __PTRDIFF_WIDTH
@@ -48,5 +59,7 @@
 
 #define SIZE_WIDTH __SIZE_WIDTH
 #define SIZE_MAX __SIZE_MAX
+
+#define BITINT_MAXWIDTH __ULLONG_WIDTH
 
 #endif /* __ABI_LIMITS_H */
