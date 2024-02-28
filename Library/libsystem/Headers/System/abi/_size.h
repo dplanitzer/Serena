8fe58beaeadfbce9e61279cddf79c1f1f6cc851c
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2024-02-27 18:45:44 -0800

    Cleanup of _size.h

--- Library/libsystem/Headers/System/abi/_size.h
+++ Library/libsystem/Headers/System/abi/_size.h
@@ -1,16 +1,16 @@
 //
-//  _sizedef.h
+//  _size.h
 //  libsystem
 //
 //  Created by Dietmar Planitzer on 8/30/23.
 //  Copyright © 2023 Dietmar Planitzer. All rights reserved.
 //
 
-#ifndef __SYS_SIZEDEF_H
-#define __SYS_SIZEDEF_H 1
+#ifndef __SYS_SIZE_H
+#define __SYS_SIZE_H 1
 
 #include <System/abi/_dmdef.h>
 
 typedef __size_t size_t;
 
-#endif /* __SYS_SIZEDEF_H */
+#endif /* __SYS_SIZE_H */
