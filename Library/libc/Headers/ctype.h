b89a4cdecfa62700404681bc9acb7341d1e89180
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2024-03-24 22:15:18 -0700

    ctype supports function and macro definitions now

--- Library/libc/Headers/ctype.h
+++ Library/libc/Headers/ctype.h
@@ -29,6 +29,85 @@ extern int ispunct(int ch);
 extern int tolower(int ch);
 extern int toupper(int ch);
 
+
+extern const unsigned short __gCharClasses[128];
+
+#define __IS_ALPHA  ((unsigned short) 1)
+#define __IS_LOWER  ((unsigned short) 2)
+#define __IS_UPPER  ((unsigned short) 4)
+#define __IS_DIGIT  ((unsigned short) 8)
+#define __IS_XDIGIT ((unsigned short) 16)
+#define __IS_CNTRL  ((unsigned short) 32)
+#define __IS_SPACE  ((unsigned short) 64)
+#define __IS_BLANK  ((unsigned short) 128)
+#define __IS_PRINT  ((unsigned short) 256)
+#define __IS_PUNCT  ((unsigned short) 512)
+#define __IS_GRAPH  ((unsigned short) 1024)
+#define __IS_ALNUM  ((unsigned short) 2048)
+
+
+#define _isalnum(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_ALNUM) == __IS_ALNUM) ? -1 : 0 : 0)
+
+#define _isalpha(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_ALPHA) == __IS_ALPHA) ? -1 : 0 : 0)
+
+#define _islower(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_LOWER) == __IS_LOWER) ? -1 : 0 : 0)
+
+#define _isupper(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_UPPER) == __IS_UPPER) ? -1 : 0 : 0)
+
+#define _isdigit(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_DIGIT) == __IS_DIGIT) ? -1 : 0 : 0)
+
+#define _isxdigit(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_XDIGIT) == __IS_XDIGIT) ? -1 : 0 : 0)
+
+#define _iscntrl(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_CNTRL) == __IS_CNTRL) ? -1 : 0 : 0)
+
+#define _isgraph(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_GRAPH) == __IS_GRAPH) ? -1 : 0 : 0)
+
+#define _isspace(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_SPACE) == __IS_SPACE) ? -1 : 0 : 0)
+
+#define _isblank(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_BLANK) == __IS_BLANK) ? -1 : 0 : 0)
+
+#define _isprint(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_PRINT) == __IS_PRINT) ? -1 : 0 : 0)
+
+#define _ispunct(ch) \
+((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_PUNCT) == __IS_PUNCT) ? -1 : 0 : 0)
+
+
+#define _tolower(ch) \
+((((ch) & 0xffffff80) == 0 && ((__gCharClasses[ch] & __IS_UPPER) == __IS_UPPER)) ? (ch) + 32 : ch)
+
+#define _toupper(ch) \
+((((ch) & 0xffffff80) == 0 && ((__gCharClasses[ch] & __IS_LOWER) == __IS_LOWER)) ? (ch) - 32 : ch)
+
+
+#ifndef _CTYPE_NO_MACROS
+#define isalnum(ch) _isalnum(ch)
+#define isalpha(ch) _isalpha(ch)
+#define islower(ch) _islower(ch)
+#define isupper(ch) _isupper(ch)
+#define isdigit(ch) _isdigit(ch)
+#define isxdigit(ch) _isxdigit(ch)
+#define iscntrl(ch) _iscntrl(ch)
+#define isgraph(ch) _isgraph(ch)
+#define isspace(ch) _isspace(ch)
+#define isblank(ch) _isblank(ch)
+#define isprint(ch) _isprint(ch)
+#define ispunct(ch) _ispunct(ch)
+
+#define tolower(ch) _tolower(ch)
+#define toupper(ch) _toupper(ch)
+#endif /* _CTYPE_NO_MACROS */
+
 __CPP_END
 
 #endif /* _CTYPE_H */
--- Library/libc/Sources/ctype.c
+++ Library/libc/Sources/ctype.c
@@ -6,273 +6,207 @@
 //  Copyright © 2023 Dietmar Planitzer. All rights reserved.
 //
 
+#define _CTYPE_NO_MACROS
 #include <ctype.h>
 
-#define IS_ALPHA    ((unsigned short) 1)
-#define IS_LOWER    ((unsigned short) 2)
-#define IS_UPPER    ((unsigned short) 4)
-#define IS_DIGIT    ((unsigned short) 8)
-#define IS_XDIGIT   ((unsigned short) 16)
-#define IS_CNTRL    ((unsigned short) 32)
-#define IS_SPACE    ((unsigned short) 64)
-#define IS_BLANK    ((unsigned short) 128)
-#define IS_PRINT    ((unsigned short) 256)
-#define IS_PUNCT    ((unsigned short) 512)
-#define IS_GRAPH    ((unsigned short) 1024)
-#define IS_ALNUM    ((unsigned short) 2048)
-
 // <https://en.cppreference.com/w/c/string/byte>
-static const unsigned short gCharClasses[128] = {
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,                                                           // 0x08
-    IS_CNTRL|IS_SPACE|IS_BLANK,
-    IS_CNTRL|IS_SPACE,
-    IS_CNTRL|IS_SPACE,
-    IS_CNTRL|IS_SPACE,
-    IS_CNTRL|IS_SPACE,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,                                                           // 0x10
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_CNTRL,
-    IS_PRINT|IS_SPACE|IS_BLANK,                                         // 0x20
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_DIGIT|IS_XDIGIT,                      // 0x30
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_DIGIT|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_DIGIT|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_DIGIT|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_DIGIT|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_DIGIT|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_DIGIT|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_DIGIT|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_DIGIT|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_DIGIT|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,                                         // 0x40
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,                       // 0x50
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_UPPER,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,                                         // 0x60
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER|IS_XDIGIT,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,                       // 0x70
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_ALNUM|IS_ALPHA|IS_LOWER,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_PRINT|IS_GRAPH|IS_PUNCT,
-    IS_CNTRL                                                            // 0x7f
+const unsigned short __gCharClasses[128] = {
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,                                                         // 0x08
+    __IS_CNTRL|__IS_SPACE|__IS_BLANK,
+    __IS_CNTRL|__IS_SPACE,
+    __IS_CNTRL|__IS_SPACE,
+    __IS_CNTRL|__IS_SPACE,
+    __IS_CNTRL|__IS_SPACE,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,                                                         // 0x10
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_CNTRL,
+    __IS_PRINT|__IS_SPACE|__IS_BLANK,                                   // 0x20
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_DIGIT|__IS_XDIGIT,            // 0x30
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_DIGIT|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_DIGIT|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_DIGIT|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_DIGIT|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_DIGIT|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_DIGIT|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_DIGIT|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_DIGIT|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_DIGIT|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,                                   // 0x40
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,             // 0x50
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_UPPER,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,                                   // 0x60
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER|__IS_XDIGIT,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,             // 0x70
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_ALNUM|__IS_ALPHA|__IS_LOWER,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_PRINT|__IS_GRAPH|__IS_PUNCT,
+    __IS_CNTRL                                                          // 0x7f
 };
 
 int isalnum(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_ALNUM) == IS_ALNUM) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _isalnum(ch);
 }
 
 int isalpha(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_ALPHA) == IS_ALPHA) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _isalpha(ch);
 }
 
 int islower(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_LOWER) == IS_LOWER) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _islower(ch);
 }
 
 int isupper(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_UPPER) == IS_UPPER) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _isupper(ch);
 }
 
 int isdigit(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_DIGIT) == IS_DIGIT) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _isdigit(ch);
 }
 
 int isxdigit(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_XDIGIT) == IS_XDIGIT) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _isxdigit(ch);
 }
 
 int iscntrl(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_CNTRL) == IS_CNTRL) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _iscntrl(ch);
 }
 
 int isgraph(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_GRAPH) == IS_GRAPH) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _isgraph(ch);
 }
 
 int isspace(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_SPACE) == IS_SPACE) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _isspace(ch);
 }
 
 int isblank(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_BLANK) == IS_BLANK) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _isblank(ch);
 }
 
 int isprint(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_PRINT) == IS_PRINT) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _isprint(ch);
 }
 
 int ispunct(int ch)
 {
-    if (ch >= 0 && ch < 128) {
-        return ((gCharClasses[ch] & IS_PUNCT) == IS_PUNCT) ? -1 : 0;
-    } else {
-        return 0;
-    }
+    return _ispunct(ch);
 }
 
 int tolower(int ch)
 {
-    if (ch >= 0 && ch < 128 && ((gCharClasses[ch] & IS_UPPER) == IS_UPPER)) {
-        return ch + 32;
-    }
-    return ch;
+    return _tolower(ch);
 }
 
 int toupper(int ch)
 {
-    if (ch >= 0 && ch < 128 && ((gCharClasses[ch] & IS_LOWER) == IS_LOWER)) {
-        return ch - 32;
-    }
-    return ch;
+    return _toupper(ch);
 }
