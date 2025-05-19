ad83ffcbc0ea9d551020b761a833b67249fe0eaf
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2025-05-18 19:42:59 -0700

    Cleanup

--- Commands/copy.c
+++ Commands/copy.c
@@ -8,12 +8,19 @@
 
 #include <clap.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
+#include <unistd.h>
 #include <sys/perm.h>
 #include <sys/stat.h>
+
+
+#define COPY_BUF_SIZE   1024
+
+char path_buf[PATH_MAX];
+char copy_buf[COPY_BUF_SIZE];
 
 
 const char* src_path = "";
@@ -29,127 +36,141 @@ CLAP_DECL(params,
 );
 
 
-static errno_t _copy_file(const char* _Nonnull srcPath, const char* _Nonnull dstPath)
+static int copy_file_contents(int sfd, int dfd)
 {
-    decl_try_err();
-    const size_t bufSize = 1024;
-    char* buf = NULL;
-    int sfd = -1, dfd = -1;
-    struct stat info;
-    mode_t perms;
     ssize_t nBytesRead, nBytesWritten;
+    int err;
 
-    try_null(buf, malloc(bufSize), ENOMEM);
-
-
-    try(open(srcPath, O_RDONLY, &sfd));
-    if (fstat(sfd, &info) != 0) {
-        throw(errno);
-    }
-    perms = S_FPERM(info.st_mode);
-    perm_add(perms, S_ICUSR, S_IW);
-    try(creat(dstPath, O_WRONLY|O_TRUNC, perms, &dfd));
-
-    while (err == EOK) {
-        err = read(sfd, buf, bufSize, &nBytesRead);
-        if (nBytesRead == 0 || err != EOK) {
-            break;
+    for (;;) {
+        err = read(sfd, copy_buf, COPY_BUF_SIZE, &nBytesRead);
+        if (err != 0 || nBytesRead == 0) {
+            return (err == 0) ? 0 : -1;
         }
 
-        err = write(dfd, buf, nBytesRead, &nBytesWritten);
+        err = write(dfd, copy_buf, nBytesRead, &nBytesWritten);
+        if (err != 0) {
+            return -1;
+        }
     }
 
-    if (!perm_has(info.st_mode, S_ICUSR, S_IW)) {
+    return 0;
+}
+
+static int copy_file(const char* _Nonnull srcPath, const struct stat* _Nonnull srcStat, const char* _Nonnull dstPath)
+{
+    int sfd = -1, dfd = -1;
+    mode_t perms = S_FPERM(srcStat->st_mode);
+    int err;
+
+    // Need to ensure that the destination file has write permissions so that we
+    // can actually copy the data
+    perm_add(perms, S_ICUSR, S_IW);
+
+
+    err = open(srcPath, O_RDONLY, &sfd);
+    if (err == 0) {
+        err = creat(dstPath, O_WRONLY|O_TRUNC, perms, &dfd);
+    }
+
+    if (sfd != -1 && dfd != -1) {
+        if (copy_file_contents(sfd, dfd) != 0) {
+            //XXX a funlink() would be nice here...
+            unlink(dstPath);
+            close(dfd);
+            dfd = -1;
+        }
+    }
+
+
+    // Remove the write rights from the destination if the source doesn't have
+    // write rights
+    if (dfd != -1 && !perm_has(srcStat->st_mode, S_ICUSR, S_IW)) {
         //XXX use fchmod() instead once it exists
-        chmod(dstPath, info.st_mode);
+        chmod(dstPath, srcStat->st_mode);
     }
 
 
-catch:
     if (dfd != -1) {
         close(dfd);
     }
     if (sfd != -1) {
         close(sfd);
     }
-    free(buf);
-    return err;
+
+    return (sfd != -1 && dfd != -1) ? 0 : -1;
 }
 
-static errno_t copy_file(const char* _Nonnull srcPath, const char* _Nonnull dstPath)
+static const char* _Nullable make_path(const char* _Nonnull dirPath, const char* _Nonnull lastPathComponent)
 {
-    decl_try_err();
-    struct stat info;
-    char* dpath;
+    const size_t dstPathLen = strlen(dirPath);
+    const size_t lastPathComponentLen = strlen(lastPathComponent);
+    const size_t hasSlash = (dirPath[dstPathLen - 1] == '/') ? 1 : 0;
+    const size_t pathLen = dstPathLen + hasSlash + lastPathComponentLen;
+    char* p = path_buf;
 
-    if (stat(srcPath, &info) == 0) {
-        if (!S_ISREG(info.st_mode)) {
-            return EINVAL;
-        }
-    }
-    else {
-        return errno;
+    if (pathLen > PATH_MAX - 1) {
+        errno = EINVAL;
+        return NULL;
     }
 
-
-    if (stat(dstPath, &info) == 0) {
-        if (S_ISDIR(info.st_mode)) {
-            dpath = malloc(PATH_MAX);
-            if (dpath == NULL) {
-                return ENOMEM;
-            }
-
-            const size_t dstPathLen = strlen(dstPath);
-            const char* lastSrcPathComponent = strrchr(srcPath, '/');
-            const char* fname = (lastSrcPathComponent) ? lastSrcPathComponent + 1 : srcPath;
-            const size_t fnameLen = strlen(fname);
-            const size_t hasSlash = (dstPath[dstPathLen - 1] == '/') ? 1 : 0;
-            const size_t dpathLen = dstPathLen + hasSlash + fnameLen;
-            char* p = dpath;
-
-            if (dpathLen > PATH_MAX - 1) {
-                return EINVAL;
-            }
-
-            memcpy(p, dstPath, dstPathLen);
-            p += dstPathLen;
-            if (!hasSlash) {
-                *p++ = '/';
-            }
-            memcpy(p, fname, fnameLen);
-        }
-        else if (S_ISREG(info.st_mode)) {
-            dpath = (char*)dstPath;
-        }
-        else {
-            return EINVAL;
-        }
+    memcpy(p, dirPath, dstPathLen);
+    p += dstPathLen;
+    if (!hasSlash) {
+        *p++ = '/';
     }
-    else {
-        dpath = (char*)dstPath;
-    }
-
+    memcpy(p, lastPathComponent, lastPathComponentLen);
     
-    err = _copy_file(srcPath, dpath);
-    if (dpath != dstPath) {
-        free(dpath);
+    return path_buf;
+}
+
+static int copy_obj(const char* _Nonnull srcPath, const char* _Nonnull dstPath)
+{
+    struct stat srcstat, dststat;
+    bool hasSrc = false, hasDst = false;
+
+    if (stat(srcPath, &srcstat) == 0) {
+        hasSrc = true;
+    }
+    if (stat(dstPath, &dststat) == 0) {
+        hasDst = true;
     }
 
-    return err;
+
+    // Source must be a file for now
+    if (!hasSrc || !S_ISREG(srcstat.st_mode)) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    // Destination must not exist or be a directory
+    if (hasDst && !S_ISDIR(dststat.st_mode)) {
+        errno = EINVAL;
+        return -1;
+    }
+
+
+    // Compute the final destination path if 'dstPath' points to a directory
+    if (hasDst && S_ISDIR(dststat.st_mode)) {
+        const char* lastSrcPathComponent = strrchr(srcPath, '/');
+        const char* fileName = (lastSrcPathComponent) ? lastSrcPathComponent + 1 : srcPath;
+
+        dstPath = make_path(dstPath, fileName);
+    }
+
+
+    return copy_file(srcPath, &srcstat, dstPath);
 }
 
 
 int main(int argc, char* argv[])
 {
-    decl_try_err();
-
     clap_parse(0, params, argc, argv);
 
-    
-    err = copy_file(src_path, dst_path);
-    if (err != EOK) {
-        clap_error(argv[0], "%s: %s", src_path, strerror(err));
+    if (copy_obj(src_path, dst_path) == 0) {
+        return EXIT_SUCCESS;
     }
-
-    return (err == EOK) ? EXIT_SUCCESS : EXIT_FAILURE;
+    else {
+        clap_error(argv[0], "%s: %s", src_path, strerror(errno));
+        return EXIT_FAILURE;
+    }
 }
