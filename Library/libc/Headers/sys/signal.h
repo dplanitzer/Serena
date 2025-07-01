d0772dd7421c534177b9bea45f4e8cbdb9aa6d2e
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2025-06-30 22:02:29 -0700

    Updated comments

--- Library/libc/Headers/sys/signal.h
+++ Library/libc/Headers/sys/signal.h
@@ -16,21 +16,19 @@
 
 __CPP_BEGIN
 
-// Waits on the wait queue if no signals are pending. Wakes up as soon as a signal
-// is sent to the wait queue. Returns the pending signals in 'psigs'. Note that
-// this calls consumes all pending signals.
+// Checks whether signals are pending and blocks the caller until at least one
+// signal arrives. Which signals this function waits for is controlled by the
+// signal mask of the calling thread of execution. Returns all pending signals
+// in 'psigs' and resets the internal pending signal state.
 extern int sig_wait(unsigned int* _Nullable psigs);
 
-// Waits on the wait queue until another vcpu calls wakeup() on the queue or
-// until the timeout 'wtp' is reached. Whatever comes first. 'wtp' is by default
-// interpreted as a duration that will be added to the current time. Pass
+// Like sig_wait() but limits the waiting time to the timeout 'wtp'. 'wtp' is by
+// default interpreted as a duration that will be added to the current time. Pass
 // TIMER_ABSTIME if 'wtp' should be interpreted as an absolute point in time
 // instead.
 extern int sig_timedwait(int flags, const struct timespec* _Nonnull wtp, unsigned int* _Nullable psigs);
 
-// Sends signals to all vcpus waiting on the wait queue. 'sigs' is the set of
-// signals that should be sent. These signals are merged into the pending signal
-// set.
+// Sends the signal 'signo' to the vcpu 'vcpu'.
 extern int sig_raise(int vcpu, int signo);
 
 
