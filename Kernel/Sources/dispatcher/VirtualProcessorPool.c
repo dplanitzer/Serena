fd60c7907870253cdfa75f09c4d0e6408c3031b4
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2025-07-04 21:26:34 -0700

    Simplification

--- Kernel/Sources/dispatcher/VirtualProcessorPool.c
+++ Kernel/Sources/dispatcher/VirtualProcessorPool.c
@@ -7,17 +7,15 @@
 //
 
 #include "VirtualProcessorPool.h"
-#include "Lock.h"
 #include "VirtualProcessorScheduler.h"
+#include "Lock.h"
 #include <kern/kalloc.h>
 
 
 #define REUSE_CACHE_CAPACITY    16
 typedef struct VirtualProcessorPool {
     Lock    lock;
-    List    inuse_queue;        // VPs in use
     List    reuse_queue;        // VPs available for reuse
-    int     inuse_count;        // count of VPs that are in use
     int     reuse_count;        // count of how many VPs are in the reuse queue
     int     reuse_capacity;     // reuse cache will not store more than this. If a VP exits while the cache is at max capacity -> VP will exit for good and get finalized
 } VirtualProcessorPool;
@@ -32,7 +30,6 @@ errno_t VirtualProcessorPool_Create(VirtualProcessorPoolRef _Nullable * _Nonnull
     VirtualProcessorPool* self;
     
     try(kalloc_cleared(sizeof(VirtualProcessorPool), (void**) &self));
-    List_Init(&self->inuse_queue);
     List_Init(&self->reuse_queue);
     Lock_Init(&self->lock);
     self->reuse_capacity = REUSE_CACHE_CAPACITY;
@@ -48,7 +45,6 @@ catch:
 void VirtualProcessorPool_Destroy(VirtualProcessorPoolRef _Nullable self)
 {
     if (self) {
-        List_Deinit(&self->inuse_queue);
         List_Deinit(&self->reuse_queue);
         Lock_Deinit(&self->lock);
         kfree(self);
@@ -66,6 +62,8 @@ errno_t VirtualProcessorPool_AcquireVirtualProcessor(VirtualProcessorPoolRef _No
     List_ForEach(&self->reuse_queue, ListNode, {
         VirtualProcessor* cvp = VP_FROM_OWNER_NODE(pCurNode);
 
+        // Make sure that the VP is suspended at this point. It may still be in
+        // the process of finishing the suspend. See relinquish() below
         if (VirtualProcessor_IsSuspended(cvp)) {
             vp = cvp;
             break;
@@ -75,9 +73,6 @@ errno_t VirtualProcessorPool_AcquireVirtualProcessor(VirtualProcessorPoolRef _No
     if (vp) {
         List_Remove(&self->reuse_queue, &vp->owner_qe);
         self->reuse_count--;
-        
-        List_InsertBeforeFirst(&self->inuse_queue, &vp->owner_qe);
-        self->inuse_count++;
     }
     
     Lock_Unlock(&self->lock);
@@ -86,11 +81,6 @@ errno_t VirtualProcessorPool_AcquireVirtualProcessor(VirtualProcessorPoolRef _No
     // Create a new VP if we were not able to reuse a cached one
     if (vp == NULL) {
         try(VirtualProcessor_Create(&vp));
-
-        Lock_Lock(&self->lock);
-        List_InsertBeforeFirst(&self->inuse_queue, &vp->owner_qe);
-        self->inuse_count++;
-        Lock_Unlock(&self->lock);
     }
     
     
@@ -101,11 +91,8 @@ errno_t VirtualProcessorPool_AcquireVirtualProcessor(VirtualProcessorPoolRef _No
     VirtualProcessor_SetPriority(vp, params.priority);
     try(VirtualProcessor_SetClosure(vp, VirtualProcessorClosure_Make(params.func, params.context, params.kernelStackSize, params.userStackSize)));
 
-    *pOutVP = vp;
-    return EOK;
-
 catch:
-    *pOutVP = NULL;
+    *pOutVP = vp;
     return err;
 }
 
@@ -114,7 +101,7 @@ catch:
 // instead. Note that the VP is suspended in any case.
 _Noreturn VirtualProcessorPool_RelinquishVirtualProcessor(VirtualProcessorPoolRef _Nonnull self, VirtualProcessor* _Nonnull vp)
 {
-    bool didReuse = false;
+    bool doReuse = false;
 
     // Null out the dispatch queue reference in any case since the VP should no
     // longer be associated with a queue.
@@ -123,21 +110,18 @@ _Noreturn VirtualProcessorPool_RelinquishVirtualProcessor(VirtualProcessorPoolRe
 
     // Try to cache the VP
     Lock_Lock(&self->lock);
-    
-    List_Remove(&self->inuse_queue, &vp->owner_qe);
-    self->inuse_count--;
 
     if (self->reuse_count < self->reuse_capacity) {
         List_InsertBeforeFirst(&self->reuse_queue, &vp->owner_qe);
         self->reuse_count++;
-        didReuse = true;
+        doReuse = true;
     }
     Lock_Unlock(&self->lock);
     
 
     // Suspend the VP if we decided to reuse it and schedule it for finalization
     // (termination) otherwise.
-    if (didReuse) {
+    if (doReuse) {
         try_bang(VirtualProcessor_Suspend(vp));
     } else {
         VirtualProcessor_Terminate(vp);
