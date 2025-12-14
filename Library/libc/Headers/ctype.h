8809c56fa4c092aeb5e8b16bf15ee15dd3200125
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2025-12-13 16:54:50 -0800

    Streamlined ctype.h

--- Library/libc/Headers/ctype.h
+++ Library/libc/Headers/ctype.h
@@ -30,64 +30,62 @@ extern int tolower(int ch);
 extern int toupper(int ch);
 
 
-extern const unsigned short __gCharClasses[128];
+extern const unsigned short __C_LOCALE_CC[128];
 
-#define __IS_ALPHA  ((unsigned short) 1)
-#define __IS_LOWER  ((unsigned short) 2)
-#define __IS_UPPER  ((unsigned short) 4)
-#define __IS_DIGIT  ((unsigned short) 8)
-#define __IS_XDIGIT ((unsigned short) 16)
-#define __IS_CNTRL  ((unsigned short) 32)
-#define __IS_SPACE  ((unsigned short) 64)
-#define __IS_BLANK  ((unsigned short) 128)
-#define __IS_PRINT  ((unsigned short) 256)
-#define __IS_PUNCT  ((unsigned short) 512)
-#define __IS_GRAPH  ((unsigned short) 1024)
-#define __IS_ALNUM  ((unsigned short) 2048)
+#define __IS_UPPER  ((unsigned short) 0x001)
+#define __IS_LOWER  ((unsigned short) 0x002)
+#define __IS_DIGIT  ((unsigned short) 0x004)
+#define __IS_XDIGIT ((unsigned short) 0x008)
+#define __IS_CNTRL  ((unsigned short) 0x010)
+#define __IS_SPACE  ((unsigned short) 0x020)
+#define __IS_BLANK  ((unsigned short) 0x040)
+#define __IS_PRINT  ((unsigned short) 0x080)
+#define __IS_PUNCT  ((unsigned short) 0x100)
+#define __IS_GRAPH  ((unsigned short) 0x200)
 
 
 #define _isalnum(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_ALNUM) == __IS_ALNUM) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & (__IS_UPPER|__IS_LOWER|__IS_DIGIT)) != 0) ? -1 : 0 : 0)
 
 #define _isalpha(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_ALPHA) == __IS_ALPHA) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & (__IS_UPPER|__IS_LOWER)) != 0) ? -1 : 0 : 0)
 
 #define _islower(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_LOWER) == __IS_LOWER) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & __IS_LOWER) == __IS_LOWER) ? -1 : 0 : 0)
 
 #define _isupper(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_UPPER) == __IS_UPPER) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & __IS_UPPER) == __IS_UPPER) ? -1 : 0 : 0)
 
 #define _isdigit(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_DIGIT) == __IS_DIGIT) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & __IS_DIGIT) == __IS_DIGIT) ? -1 : 0 : 0)
 
 #define _isxdigit(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_XDIGIT) == __IS_XDIGIT) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & __IS_XDIGIT) == __IS_XDIGIT) ? -1 : 0 : 0)
 
 #define _iscntrl(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_CNTRL) == __IS_CNTRL) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & __IS_CNTRL) == __IS_CNTRL) ? -1 : 0 : 0)
 
 #define _isgraph(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_GRAPH) == __IS_GRAPH) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & __IS_GRAPH) == __IS_GRAPH) ? -1 : 0 : 0)
 
 #define _isspace(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_SPACE) == __IS_SPACE) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & __IS_SPACE) == __IS_SPACE) ? -1 : 0 : 0)
 
 #define _isblank(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_BLANK) == __IS_BLANK) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & __IS_BLANK) == __IS_BLANK) ? -1 : 0 : 0)
 
 #define _isprint(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_PRINT) == __IS_PRINT) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & __IS_PRINT) == __IS_PRINT) ? -1 : 0 : 0)
 
 #define _ispunct(ch) \
-((((ch) & 0xffffff80) == 0) ? ((__gCharClasses[ch] & __IS_PUNCT) == __IS_PUNCT) ? -1 : 0 : 0)
+(((unsigned int)(ch) < 128) ? ((__C_LOCALE_CC[ch] & __IS_PUNCT) == __IS_PUNCT) ? -1 : 0 : 0)
 
 
 #define _tolower(ch) \
-((((ch) & 0xffffff80) == 0 && ((__gCharClasses[ch] & __IS_UPPER) == __IS_UPPER)) ? (ch) + 32 : ch)
+((((unsigned int)(ch) < 128) && ((__C_LOCALE_CC[ch] & __IS_UPPER) == __IS_UPPER)) ? (ch) + 32 : ch)
 
 #define _toupper(ch) \
-((((ch) & 0xffffff80) == 0 && ((__gCharClasses[ch] & __IS_LOWER) == __IS_LOWER)) ? (ch) - 32 : ch)
+((((unsigned int)(ch) < 128) && ((__C_LOCALE_CC[ch] & __IS_LOWER) == __IS_LOWER)) ? (ch) - 32 : ch)
 
 __CPP_END
 
