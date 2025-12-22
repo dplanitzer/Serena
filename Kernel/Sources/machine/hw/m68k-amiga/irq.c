2c921144dfb330840dbe868f5cd4d3dcdc871dcb
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2025-12-21 16:37:30 -0800

    Cleanup

--- Kernel/Sources/machine/hw/m68k-amiga/irq.c
+++ Kernel/Sources/machine/hw/m68k-amiga/irq.c
@@ -27,6 +27,114 @@ void* _Nullable     g_irq_key_arg;
 irq_direct_func_t   g_irq_disk_block_func = (irq_direct_func_t)_nop_irq_handler;
 void* _Nullable     g_irq_disk_block_arg;
 
+size_t  g_irq_stat_uninit;
+size_t  g_irq_stat_spurious;
+size_t  g_irq_stat_nmi;
+
+
+size_t irq_get_stat(int stat_id)
+{
+    switch (stat_id) {
+        case IRQ_STAT_UNINITIALIZED_COUNT:
+            return g_irq_stat_uninit;
+
+        case IRQ_STAT_SPURIOUS_COUNT:
+            return g_irq_stat_spurious;
+
+        case IRQ_STAT_NON_MASKABLE_COUNT:
+            return g_irq_stat_nmi;
+
+        default:
+            return 0;
+    }
+}
+
+
+// irq_handlers_for_id() is defined in the platform specific irq.c file
+void irq_add_handler(irq_handler_t* _Nonnull h)
+{
+    const unsigned sim = irq_set_mask(IRQ_MASK_ALL);
+
+    irq_handler_t** listp = irq_handlers_for_id(h->id);
+    register irq_handler_t* ph = NULL;
+    register irq_handler_t* ch = *listp;
+
+    while (ch) {
+        if (ch->priority > h->priority) {
+            break;
+        }
+        
+        ph = ch;
+        ch = ch->next;
+    }
+    
+    if (ph) {
+        h->next = ph->next;
+        ph->next = h;
+    }
+    else {
+        h->next = *listp;
+        *listp = h;
+    }
+
+    irq_restore_mask(sim);
+}
+
+void irq_remove_handler(irq_handler_t* _Nullable h)
+{
+    const unsigned sim = irq_set_mask(IRQ_MASK_ALL);
+
+    irq_handler_t** listp = irq_handlers_for_id(h->id);
+    irq_handler_t* ph = NULL;
+    irq_handler_t* ch = *listp;
+    bool found = false;
+
+    while (ch) {
+        if (ch == h) {
+            found = true;
+            break;
+        }
+
+        ph = ch;
+        ch = ch->next;
+    }
+
+    if (found) {
+        if (ph) {
+            ph->next = h->next;
+        }
+        else {
+            *listp = h->next;
+        }
+        h->next = NULL;
+    }
+
+    irq_restore_mask(sim);
+}
+
+void irq_set_handler_enabled(irq_handler_t* _Nonnull h, bool enabled)
+{
+    const unsigned sim = irq_set_mask(IRQ_MASK_ALL);
+
+    h->enabled = enabled;
+    irq_restore_mask(sim);
+}
+
+// Called from the IRQ context. Run all handlers for the given interrupt list
+void _irq_run_handlers(irq_handler_t* _Nullable irq_list)
+{
+    register irq_handler_t* ch = irq_list;
+
+    while (ch) {
+        if (ch->func && ch->enabled) {
+            if (ch->func(ch->arg)) {
+                break;
+            }
+        }
+        ch = ch->next;
+    }
+}
+
 
 void irq_set_direct_handler(int irq_id, irq_direct_func_t _Nonnull f, void* _Nullable arg)
 {
