66b4986c24d5e83f7851457ddda61ad28793769b
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2025-12-28 22:19:21 -0800

    Optimized abs() functions with alternate approach

--- Library/libc/Sources/arch/m68k/abs.s
+++ Library/libc/Sources/arch/m68k/abs.s
@@ -14,16 +14,11 @@
 ;-------------------------------------------------------------------------------
 ; int abs(int n)
 ; long labs(long n)
-; based on: https://stackoverflow.com/questions/12041632/how-to-compute-the-integer-absolute-value
 _abs:
 _labs:
     cargs abs_n.l
     move.l  abs_n(sp), d0   ; d0 := n
-    move.l  d2, a0
-    move.l  d0, d2
-    moveq.l #31, d1
-    asr.l   d1, d2          ; mask = n >> 31 | d2 := mask
-    eor.l   d2, d0          ; d0 := mask ^ n
-    sub.l   d2, d0          ; r = (mask ^ n) - mask
-    move.l  a0, d2
+    bpl.s   .1
+    neg.l   d0
+.1:
     rts
--- Library/libc/Sources/arch/m68k/llabs.s
+++ Library/libc/Sources/arch/m68k/llabs.s
@@ -14,25 +14,14 @@
 ;-------------------------------------------------------------------------------
 ; long long llabs(long long n)
 ; intmax_t imaxabs(intmax_t n)
-; based on: https://stackoverflow.com/questions/12041632/how-to-compute-the-integer-absolute-value
 _llabs:
 _imaxabs:
     cargs abs_nh.l, abs_nl.l
-    move.l  abs_nh(sp), d0   ; (d0, d1) := (n_h, n_l)
     move.l  abs_nl(sp), d1
-    
-    move.l  d2, a0
-    move.l  d3, a1
+    move.l  abs_nh(sp), d0   ; (d0, d1) := (n_h, n_l)
+    bpl.l   .1
 
-    moveq.l #31, d3
-    asr.l   d3, d2          ; mask = n_h >> 31 | d2 := mask; mask_l == mask_h
-
-    eor.l   d2, d0          ; d0 := mask ^ n_h
-    eor.l   d2, d1          ; d1 := mask ^ n_l
-
-    sub.l   d2, d0          ; r_h = (mask ^ n_h) - mask
-    sub.l   d2, d1          ; r_l = (mask ^ n_l) - mask
-
-    move.l  a0, d2
-    move.l  a1, d3
+    neg.l   d1
+    negx.l  d0
+.1:
     rts
