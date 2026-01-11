d7932b3d56c1d41774bd0cd053bd1fcf0d5e6ad9
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2026-01-10 18:33:07 -0800

    Simple memset() optimization for some CPU types

--- Library/libc/Sources/string/__string.h
+++ Library/libc/Sources/string/__string.h
@@ -17,7 +17,8 @@ typedef uint32_t uword_t;
 #define WORD_SIZE       4
 #define WORD_SIZMASK    3
 #define WORD_SHIFT      2
-#define WORD_FROM_BYTE(b) ((b) << 24) | ((b) << 16) | ((b) << 8) | (b)
+#define HALFWORD_FROM_BYTE(b) (((b) << 8) | (b))
+#define WORD_FROM_BYTE(b) ((HALFWORD_FROM_BYTE(b) << 16) | HALFWORD_FROM_BYTE(b))
 #elif defined(__LLP64__) || defined(__LP64__)
 typedef uint64_t uword_t;
 #define WORD_SIZE       8
@@ -26,6 +27,11 @@ typedef uint64_t uword_t;
 #define WORD_FROM_BYTE(b) ((b) << 56) | ((b) << 48) | ((b) << 40) | ((b) << 32) | ((b) << 24) | ((b) << 16) | ((b) << 8) | (b)
 #else
 #error "unknown data model"
+#endif
+
+#if defined(__M68K__) || defined(__x86_64__) || defined(__i386__)
+// CPU just requires that a word/long word move is done with a 16bit aligned address
+#define __SIMPLE_ALIGNMENT_MODEL 1
 #endif
 
 #endif /* ___STRING_H */
--- Library/libc/Sources/string/memset.c
+++ Library/libc/Sources/string/memset.c
@@ -28,6 +28,12 @@ void* _Nonnull memset(void* _Nonnull dst, int c, size_t count)
 
 
     // Align to the next natural word size boundary
+#if defined(__SIMPLE_ALIGNMENT_MODEL)
+    if (((uintptr_t)p) & 1) {
+        *p++ = b;
+        count--;
+    }
+#else
     const size_t n_mod_bytes = ((uintptr_t)p & WORD_SIZMASK);
     if (n_mod_bytes > 0) {
         do {
@@ -36,11 +42,12 @@ void* _Nonnull memset(void* _Nonnull dst, int c, size_t count)
 
         count -= (WORD_SIZE - n_mod_bytes);
     }
-    const uint8_t* const pe = p + count;
+#endif
 
 
     // We know that we can do at least one natural word size op at this point.
     // Unroll by a factor of 4 whenever possible
+    const uint8_t* const pe = p + count;
     uword_t* pw = (uword_t*)p;
     const uword_t bw = WORD_FROM_BYTE(b);
     const uword_t* const pew = pw + (count >> WORD_SHIFT);
