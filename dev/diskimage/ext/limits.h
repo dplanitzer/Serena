0c9a1b5e71f934b57d2c4d83a0916bbe5689977c
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2026-01-28 21:39:38 -0800

    Moved the Windows specific ext/limits.h definitions to the Windows shim

--- dev/diskimage/ext/limits.h
+++ dev/diskimage/ext/limits.h
@@ -6,4 +6,31 @@
 //  Copyright © 2025 Dietmar Planitzer. All rights reserved.
 //
 
-#include <../../user/lib/libc/h/ext/limits.h>
+#ifndef _DI_LIMITS_H
+#define _DI_LIMITS_H 1
+
+#include <limits.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+
+#define OFF_MIN     0x80000000ll
+#define OFF_MAX     0x7fffffffll
+#define OFF_WIDTH   32
+
+
+#define SSIZE_MIN   0x8000000000000000l
+#define SSIZE_MAX   0x7fffffffffffffffl
+#define SSIZE_WIDTH 64
+
+// SIZE_MAX is defined in <limits.h>
+#define SIZE_WIDTH  64
+
+#elif defined (__APPLE__)
+
+// OFF_MIN, OFF_MAX, SSIZE_MIN, SSIZE_MAX are defined in <limits.h>
+// SIZE_MAX is defined in <stdint.h>
+#include <stdint.h>
+
+#endif
+
+#endif /* _DI_LIMITS_H */
--- user/lib/libc/h/ext/limits.h
+++ user/lib/libc/h/ext/limits.h
@@ -11,22 +11,6 @@
 
 #include <limits.h>
 
-#if defined(_WIN64)
-
-#define OFF_MIN     0x80000000ll
-#define OFF_MAX     0x7fffffffll
-#define OFF_WIDTH   32
-
-
-#define SSIZE_MIN   0x8000000000000000l
-#define SSIZE_MAX   0x7fffffffffffffffl
-#define SSIZE_WIDTH 64
-
-// SIZE_MAX is defined in win64/limits.h
-#define SIZE_WIDTH  64
-
-#else
-
 #define OFF_MIN  __OFF_MIN
 #define OFF_MAX  __OFF_MAX
 #define OFF_WIDTH __OFF_WIDTH
@@ -39,7 +23,5 @@
 
 #define SIZE_MAX __SIZE_MAX
 #define SIZE_WIDTH __SIZE_WIDTH
-
-#endif
 
 #endif /* _EXT_LIMITS_H */
