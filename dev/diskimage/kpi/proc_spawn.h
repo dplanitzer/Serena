906b6db00ad63a24e56e17192930d2ab9098cba3
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2026-05-21 21:50:24 -0700

    Removed no longer needed header file

--- dev/diskimage/kpi/proc_spawn.h
+++
@@ -1,9 +0,0 @@
-//
-//  kpi/proc_spawn.h
-//  diskimage
-//
-//  Created by Dietmar Planitzer on 5/15/25.
-//  Copyright © 2025 Dietmar Planitzer. All rights reserved.
-//
-
-#include <../../kern/h/kpi/proc_spawn.h>
